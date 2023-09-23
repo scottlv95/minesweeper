93076b4be7a31685b2053ba86ef731645cc7372c
Scott Loo <scottlv95@gmail.com>
2023-09-23 22:57:50 +0100

    Tetris game completed, tutorial

---
+++ src/Headers/Global.hpp
@@ -0,0 +1,6 @@
+constexpr unsigned char CELL_SIZE = 16;
+constexpr unsigned char COLUMNS = 8;
+constexpr unsigned char EFFECT_DURATION = 16;
+constexpr unsigned char FONT_HEIGHT = 16;
+constexpr unsigned char FONT_WIDTH = 8;
+constexpr unsigned char ROWS = 8;
